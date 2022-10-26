3055dfaa9fe6f0af22f138f39bc267e6286f4201
ananas <anastasiaa.kravchenkoo@gmail.com>
2022-10-26 20:49:32 +0300

    popup-add & element__group_active are added

--- test.js
+++ test.js
@@ -2,25 +2,58 @@ let content = document.querySelector(".content");
 let profileInfo = content.querySelector(".profile__info");
 let profileEditButton = profileInfo.querySelector(".profile__edit-button");
 
-let popup = document.querySelector(".popup");
-let popupCloseButton = popup.querySelector(".popup__close-button");
+let popupEdit = document.querySelector(".popup-edit");
+let popupEditCloseButton = popupEdit.querySelector(".popup-edit__close-button");
 
-function openPopup() {
-  console.log("open popup")
+let profileAddButton = content.querySelector(".profile__add-button");
+
+let popupAdd = document.querySelector(".popup-add");
+let popupAddCloseButton = popupAdd.querySelector(".popup-add__close-button");
+
+let elements = content.querySelectorAll(".element .element__group");
+
+function openPopupEdit() {
+  console.log("open popup-edit")
   // первый способ
   // popup.setAttribute("class", "popup popup_opened")
 
   // второй способ
   // console.log(popup.className)
   // console.log(popup.classList)
-  popup.classList.add("popup_opened");
+  popupEdit.classList.add("popup-edit_opened");
 }
 
-function closePopup() {
-  console.log("close popup")
-  popup.classList.remove("popup_opened");
+function closePopupEdit() {
+  console.log("close popup-edit")
+  popupEdit.classList.remove("popup-edit_opened");
+}
+
+function openPopupAdd() {
+  popupAdd.classList.add("popup-add_opened");
+}
+
+function closePopupAdd() {
+  popupAdd.classList.remove("popup-add_opened");
 }
 
 // светофор -> полицейский наблюдает за событием - когда загорается зеленый свет -> когда это произойдет он останавливает движение
-profileEditButton.addEventListener("click", openPopup); // к объекту profileEditButton прошу добавить слушатель события
-popupCloseButton.addEventListener("click", closePopup)
+profileEditButton.addEventListener("click", openPopupEdit); // к объекту profileEditButton прошу добавить слушатель события
+popupEditCloseButton.addEventListener("click", closePopupEdit);
+profileAddButton.addEventListener("click", openPopupAdd);
+popupAddCloseButton.addEventListener("click", closePopupAdd);
+
+// console.log(elements)
+for (let i = 0; i < elements.length; i = i + 1) {
+  let element = elements[i] // получаем i - ое сердечко (element)
+
+  element.addEventListener("click", function () {
+    // содержит ли сердечко класс "element__group_active" ?
+    if (element.classList.contains("element__group_active")) {
+      // если да -> удаляем класс 
+      element.classList.remove("element__group_active");
+    } else {
+      // если нет -> добавляем класс
+      element.classList.add("element__group_active");
+    }
+  });
+}
